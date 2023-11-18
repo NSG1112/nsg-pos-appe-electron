3a46a2d9131b1b66e5d2f3be488502c665877002
Naveen <naveensingh0630@gmail.com>
2023-11-18 17:51:05 +0530

    first commit

---
+++ start.js
@@ -0,0 +1,131 @@
+require('dotenv').config();
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+
+const server = require('./server');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path')
+
+// Retrieve the app version
+const appVersion = app.getVersion();
+
+// Set the feed URL for updates
+autoUpdater.setFeedURL({
+  provider: 'github',
+  owner: 'Ayuen-madyt',
+  repo: 'Quicktill',
+  token: process.env.GITHUB_TOKEN,
+  url: `https://github.com/Ayuen-madyt/Quicktill/releases/tag/v${appVersion}`,
+});
+
+autoUpdater.checkForUpdatesAndNotify();
+
+const setupEvents = require('./installers/setupEvents')
+ if (setupEvents.handleSquirrelEvent()) {
+    return;
+ }
+
+// Set the log file location
+log.transports.file.file = `${app.getPath('userData')}/quicktill.log`;
+
+// Set the log level (optional)
+log.transports.file.level = 'info'; // or 'debug', 'warn', 'error', etc.
+
+// Configure other options (optional)
+log.transports.file.format = '{h}:{i}:{s} {level} {text}';
+
+// Add logging to console (optional)
+log.transports.console.level = false; // Disable console logging
+
+// Initialize the logger
+log.catchErrors();
+
+// Usage example
+log.info('App started');
+
+const contextMenu = require('electron-context-menu');
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 1200,
+    frame: false,
+    minWidth: 1200, 
+    minHeight: 750,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
+    },
+  });
+
+  mainWindow.maximize();
+  mainWindow.show();
+
+  mainWindow.loadURL(
+    `file://${path.join(__dirname, 'index.html')}`
+  )
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+
+
+ipcMain.on('app-quit', (evt, arg) => {
+  app.quit()
+})
+
+
+ipcMain.on('app-reload', (event, arg) => {
+  mainWindow.reload();
+});
+
+
+
+contextMenu({
+  prepend: (params, browserWindow) => [
+     
+      {label: 'DevTools',
+       click(item, focusedWindow){
+        focusedWindow.toggleDevTools();
+      }
+    },
+     { 
+      label: "Reload", 
+        click() {
+          mainWindow.reload();
+      } 
+    // },
+    // {  label: 'Quit',  click:  function(){
+    //    mainWindow.destroy();
+    //     mainWindow.quit();
+    // } 
+  }  
+  ],
+
+});
+
+ 
+
+ 
